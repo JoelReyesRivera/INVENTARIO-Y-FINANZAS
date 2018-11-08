052ea5c410e7cc9853b9dc7c0e019b189dd95e9a
Jasiel <jasiel24pf@gmail.com>
2018-11-07 21:21:13 -0700

    Prueba ,Fecha ,manejaVentas
    Primer Push #ALaOtraArreglos :( el oliver hara el modificar

--- InventarioYFinanzas/src/ClasesBase/Venta.java
+++ InventarioYFinanzas/src/ClasesBase/Venta.java
@@ -4,7 +4,51 @@ package ClasesBase;
  * @author jasiel
  */
 public class Venta {
-    private String fecha;
-    private Cliente cliente;
-    
+    private String ClaveVta;
+    private int ClaveArt;
+    private int Cantidad;
+    private double PrecioUni;
+
+    public Venta(String ClaveVta, int ClaveArt, int Cantidad, double PrecioUni) {
+        this.ClaveVta = ClaveVta;
+        this.ClaveArt = ClaveArt;
+        this.Cantidad = Cantidad;
+        this.PrecioUni = PrecioUni;
+    }
+
+    public String getClaveVta() {
+        return ClaveVta;
+    }
+
+    public int getClaveAt() {
+        return ClaveArt;
+    }
+
+    public int getCantidad() {
+        return Cantidad;
+    }
+
+    public double getPrecioUni() {
+        return PrecioUni;
+    }
+
+    public void setClaveVta(String ClaveVta) {
+        this.ClaveVta = ClaveVta;
+    }
+
+    public void setClaveArt(int ClaveArt) {
+        this.ClaveArt = ClaveArt;
+    }
+
+    public void setCantidad(int Cantidad) {
+        this.Cantidad = Cantidad;
+    }
+
+    public void setPrecioUni(int precio) {
+        PrecioUni = precio;
+    }
+
+    public String toString() {
+        return ("Clave de la vent :" + ClaveVta + ("\n") + "Clave Articulo :" + ClaveArt + ("\n") + "Cantidad de vendidos:" + Cantidad + ("\n") + "Precio :$" + PrecioUni);
+    }
 }
--- InventarioYFinanzas/src/Manejadoras/ManejaVentas.java
+++ InventarioYFinanzas/src/Manejadoras/ManejaVentas.java
@@ -1,9 +1,35 @@
 package Manejadoras;
 
-/**
- *
- * @author ADM-PC
- */
+
+import ClasesBase.Empleado;
+import java.util.ArrayList;
+import ClasesBase.Venta; 
+
+
 public class ManejaVentas {
+    public ArrayList <Venta> Ventas;
     
+    //CONSTRUCTOR
+     public ManejaVentas(){
+        Ventas=  new ArrayList<Venta> ();
+     }
+     
+    //Agregar
+     public void agregar(String ClaveVta, int ClaveArt, int Cantidad, double PrecioUni) {
+        Ventas.add(new Venta(ClaveVta,ClaveArt,Cantidad,PrecioUni));
+    }
+     
+    //IMPRIMIR
+   public void imprimir(){ 
+       for(int i=0;i<Ventas.size();i++){
+           System.out.println("Venta numero :"+(i + 1) + ":" + "\n" + Ventas.get(i));
+       }
+   }
+   
+   
+   
+ 
 }
+   
+   
+
